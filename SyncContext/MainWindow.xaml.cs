213307b81d9a370a3c2aeb10320bf81bc0158959
xrlogic1 <xrlogic1@gmail.com>
2024-02-05 22:15:31 +0400

    SynchronizationContext

---
+++ SyncContext/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SyncContext;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private SynchronizationContext _context;
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        _context = SynchronizationContext.Current;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        var thread = new Thread(() =>
+        {
+            Thread.Sleep(2000);
+            
+            _context.Send(o =>
+            {
+                ClickButton.Background = Brushes.Brown;
+            }, null);
+        });
+        
+        thread.Start();
+    }
+}
